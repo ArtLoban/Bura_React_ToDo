719cb97534c81a2f91947b072758d584d234dce3
Artem Loban <tymanrock@gmail.com>
2020-04-13 18:26:32 +0300

    Delete item from list on button click

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
@@ -6,29 +6,69 @@ import ItemStatusFilter from '../item-status-filter';
 import Joke from '../joke';
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3},
+            {label: 'TEst', important: true, id: 4},
+        ]
+    };
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-    { label: 'TEst', important: true, id: 4 }
-  ];
+    /**
+     * @param task_id
+     */
+    deleteItem = (task_id) => {
+        /* ВАРИАНТ I */                                                        /* ВАРИАНТ II */
+        /* C деструктуризацией объекта -> {id} */                 /* Обычный подход -> item.id */
+        let todoData = this.state.todoData.filter(({id}) => {      // let todoData = this.state.todoData.filter((item) => {
+            return task_id !== id;                                            //     return task_id !== item.id;
+        });                                                                           // });
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
+        this.setState(({state}) => {
+            return {
+                todoData: todoData
+            }
+        });
 
-      <TodoList todos={todoData} />
-      <div>
-        <Joke jokeTitle="Listen a joke"/>
-      </div>
-    </div>
-  );
-};
+        /* Выше -> мой вариант */
+        return;
+        /* Ниже -> автора. Более непонятный */
 
-export default App;
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === task_id);
+
+            // [a, b, c, d, e]
+            // [a, b,    d, e]      // Удаление элемента из массива
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+
+            return {
+                todoData: newArray
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}
+                />
+                <div>
+                    <Joke jokeTitle="Listen a joke"/>
+                </div>
+            </div>
+        );
+    }
+}
